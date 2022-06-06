e8628bbe639796228435c5b65283bbb335c138ef
Salem-2022 <101900385+Salem-2022@users.noreply.github.com>
2022-06-06 16:46:59 +0200

    removed "id" from component files

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,21 @@
 import React from "react";
 export default function Form() {
   return (
-    <form id="search-form">
+    <form className="search-form">
       <div className="row">
         <div className="col-9">
           <input
             type="text"
-            className="form-control"
+            className="form-control search-text-input"
             placeholder="Enter the city's name"
             autoComplete="off"
             autoFocus="on"
-            id="search-text-input"
           />
         </div>
         <div className="col">
           <input
             type="submit"
-            className="btn btn-primary"
-            id="searchButton"
+            className="btn btn-primary searchButton"
             value="Search"
           />
         </div>
