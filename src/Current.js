d7fb19ee05df544d8967d39e443c7f8ac458d54c
Salem-2022 <101900385+Salem-2022@users.noreply.github.com>
2022-06-13 22:54:05 +0200

    added state variables inside the jsx

--- src/Current.js
+++ src/Current.js
@@ -20,16 +20,23 @@ export default function Current() {
     setWind(response.data.wind.speed);
     setIcon(response.data.weather[0].icon);
   }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let imgUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ab06a115531350a624a8309eb2a5ab0c&untis=metric`;
+  axios.get(url).then(showWeather);
+
   return (
     <div className="row">
       <div className="col currentWeather">
         <ul>
-          <li className="currentCity">Stockholm</li>
+          <li className="currentCity">{city}</li>
           <li className="currentDay">08:10</li>
           <li className="currentWeatherEmoji">
-            ☀️
+            <img src={imgUrl} alt="icon" />
             <span className="currentTemperature">
-              19
+              {temperature}
               <sup id="degC">
                 <a href="/">℃</a>
               </sup>
@@ -43,9 +50,9 @@ export default function Current() {
       </div>
       <div className="col descriptions mt-5">
         <ul>
-          <li>Description: </li>
-          <li>Humidity: </li>
-          <li>Wind: </li>
+          <li>Description:{description} </li>
+          <li>Humidity:{humidity}%</li>
+          <li>Wind: {wind}m/s </li>
         </ul>
       </div>
 
